fb125eddb9dfe7364a5b78e0333eaa5571bf8466
chandraprakash <chandraprak33@gmail.com>
2020-09-17 19:51:47 +0530

    Updates

---
+++ precache-manifest.ee6bddb232c4578e3c4e136413713d59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f96ae2e48fc37e28bb6945d080ca7d5",
+    "url": "/VSQ-Coders-Intern-Assignment/index.html"
+  },
+  {
+    "revision": "29e9bab511a45715555f",
+    "url": "/VSQ-Coders-Intern-Assignment/static/css/main.bf438a6d.chunk.css"
+  },
+  {
+    "revision": "822df284b54b6463996e",
+    "url": "/VSQ-Coders-Intern-Assignment/static/js/2.bb282844.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VSQ-Coders-Intern-Assignment/static/js/2.bb282844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29e9bab511a45715555f",
+    "url": "/VSQ-Coders-Intern-Assignment/static/js/main.35e1c1d3.chunk.js"
+  },
+  {
+    "revision": "1b5e634517d178678a6f",
+    "url": "/VSQ-Coders-Intern-Assignment/static/js/runtime-main.744b7d83.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/VSQ-Coders-Intern-Assignment/precache-manifest.ee6bddb232c4578e3c4e136413713d59.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/VSQ-Coders-Intern-Assignment/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
