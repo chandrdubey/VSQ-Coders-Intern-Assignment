78ee0df157c91245865a41c299595bbaa966be8c
chandraprakash <chandraprak33@gmail.com>
2020-09-17 19:29:48 +0530

    depoying

--- src/componets/BookClass.js
+++ src/componets/BookClass.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import axios from "axios";
 class BookClass extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
+      parentName: "",
+      parentContactNo: "",
+      email: "",
+      nameOfChild: "",
+      ageOfChild: "",
       apiData: [],
       courseData: {},
-      courseStatus: false,
-      courseMenu: "Select",
-      x: 1,
+      courseSelect: "Select",
+      x: -1,
       timeSlot: [],
       slotDate: "1999-02-23",
       slotTime: "Select",
-      isLoading:true
+      isLoading: true,
     };
   }
 
@@ -21,7 +25,7 @@ class BookClass extends Component {
     let latestSlotDate = new Date();
     latestSlotDate.setHours(todayDate.getHours() + 4);
     const latestSlotDay = getDate(latestSlotDate);
-    // console.log(`hell ${latestSlotDay}`)
+
     this.setState({
       slotDate: latestSlotDay,
     });
@@ -36,7 +40,7 @@ class BookClass extends Component {
         this.setState({
           apiData: response.data,
           courseData: this.state.apiData[0],
-          isLoading:false
+          isLoading: false,
         });
       })
       .catch(function (error) {
@@ -44,22 +48,22 @@ class BookClass extends Component {
         console.log(error);
       });
   }
+  //this function handle the changes in select course
   handleSelectChange = (event) => {
     this.setState({
       // courseData:this.state.apiData[event.target.value-1],
       x: event.target.value,
-      courseData: this.state.apiData[event.target.value - 1],
+      courseData: this.state.apiData[event.target.value],
     });
+    if(this.state.x !== -1)
+    {
+      this.setState({
+        // courseData:this.state.apiData[event.target.value-1],
+       courseSelect: this.state.apiData[event.target.value].course_name
+      });
+    }
     console.log(event.target.value);
     console.log(this.state.x);
-
-   // console.log(this.state.courseData);
-
-    //   this.setState({
-    //     courseData: course,
-    //     courseStatus: true,
-    //     courseMenu: course.course_name
-    //   });
   };
   handleChange = (event) => {
     const { name, value } = event.target;
@@ -67,47 +71,63 @@ class BookClass extends Component {
       [name]: value,
     });
   };
-  handleMenu = (event) => {
+  handleSubmit = (event) => {
     event.preventDefault();
+    alert(
+      `${this.state.parentName} , ${this.state.parentContactNo}, ${this.state.email} ,
+      ${this.state.ageOfChild}  ,${this.state.nameOfChild}, ${this.state.courseSelect},
+      ${this.state.slotDate} , ${this.state.slotTime}`
+    );
   };
-  handleSelectDefault = () => {
-    this.setState({
-      courseData: {},
-      courseStatus: false,
-    });
-  };
+
   render() {
     console.log(Date.now());
     let todayDate = new Date();
-
+    let timeSlots = [];
     const today = getDate(todayDate);
-    //  console.log(today)
+    //Getting last day of booking
     let lastDate = new Date();
     lastDate.setDate(todayDate.getDate() + 7);
-    //  console.log(lastDate);
+
+    //converting in the format which we can use in html input type of date
     const lastDay = getDate(lastDate);
     console.log(this.state.courseData);
-    let timeSlots;
-    //   console.log(lastDay);
+
+    //Checking if we got data from API
     if (this.state.courseData && this.state.courseData.slots) {
+      //Getting time in hours and minutes
       let timeArr = getSlotInHours(this.state.courseData.slots);
-      
-      console.log(`${today} = ${this.state.slotDate}`)
-      if(today === this.state.slotDate)
-      {
-        const hour = todayDate.getHours()+4;
-        timeArr.map(time => console.log( parseInt(time.substr(0,2))))
-       
-        timeSlots = timeArr.filter(time => parseInt(time.substr(0,2)) >= hour )
+      //Now checking if slot day is today. if it is then weill show the slot 4 hour after from now
+      if (today === this.state.slotDate) {
+        const hour = todayDate.getHours() + 4;
+
+        timeSlots = timeArr.filter(
+          (time) => parseInt(time.slotStart.substr(0, 2)) >= hour
+        );
         console.log(`${timeSlots} hard`);
       }
-     
+      //if booking day is last then only slot will show which are before the present time
+      else if (lastDay === this.state.slotDate) {
+        const hour = lastDate.getHours();
+        timeArr.map((time) =>
+          console.log(parseInt(time.slotStart.substr(0, 2)))
+        );
+        timeSlots = timeArr.filter(
+          (time) => parseInt(time.substr(0, 2)) <= hour
+        );
+      }
+      //all slot will show
+      else {
+        timeSlots = timeArr;
+      }
     }
 
-    return this.state.isLoading ?(<div> Loadding</div>) : (
+    return this.state.isLoading ? (
+      <div> Loadding</div>
+    ) : (
       <div>
-        <h2 className="text-center">Book your trial class today </h2>
-        <form className="mx-auto">
+        <h2 className="text-center">Book a trial class </h2>
+        <form className="mx-auto" onSubmit={this.handleSubmit}>
           <div className="form-group ">
             <label htmlFor="exampleInputEmail1">Parent Name</label>
             <input
@@ -116,6 +136,9 @@ class BookClass extends Component {
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="Parent Name"
+              name="parentName"
+              value={this.state.parentName}
+              onChange={this.handleChange}
             />
           </div>
           <div className="form-group ">
@@ -126,6 +149,9 @@ class BookClass extends Component {
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="Parent Contact Number"
+              name="parentContactNo"
+              value={this.state.parentContactNo}
+              onChange={this.handleChange}
             />
           </div>
 
@@ -137,6 +163,9 @@ class BookClass extends Component {
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="Enter email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
             />
           </div>
           <div className="form-group ">
@@ -147,6 +176,9 @@ class BookClass extends Component {
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="Child Name"
+              name="nameOfChild"
+              value={this.state.nameOfChild}
+              onChange={this.handleChange}
             />
           </div>
 
@@ -158,6 +190,9 @@ class BookClass extends Component {
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="Age of Child"
+              name="ageOfChild"
+              value={this.state.ageOfChild}
+              onChange={this.handleChange}
             />
           </div>
           <div className="form-group  course-select">
@@ -168,16 +203,22 @@ class BookClass extends Component {
               value={this.state.x}
               onChange={this.handleSelectChange}
             >
-              <option value="1">Scratch Junior</option>
-              <option value="2">Game Development</option>
-              <option value="3">App Development</option>
-              <option value="4">Web Development</option>
+              <option value="-1">Select</option>
+              {this.state.apiData.length > 0 &&
+                this.state.apiData.map((data, index) => (
+                  <option value={index} key={data.course_id}>
+                    {data.course_name}
+                  </option>
+                ))}
             </select>
           </div>
-          <div className="form-group ">
+          <div className="form-group  course-select">
+          <label htmlFor="exampleInputdate">Select Date</label>
             <input
               type="date"
               name="slotDate"
+              id="exampleInputdate"
+              className="form-control"
               value={this.state.slotDate}
               min={today}
               max={lastDay}
@@ -185,19 +226,26 @@ class BookClass extends Component {
             />
           </div>
           <div className="form-group  course-select">
-            <label htmlFor="inputState">Select Course</label>
+            <label htmlFor="inputState">Book Trial Class</label>
             <select
               id="inputState"
               className="form-control"
+              name="slotTime"
               value={this.state.slotTime}
-              onChange={this.handleSelectChange}
+              onChange={this.handleChange}
             >
-             
-             { timeSlots ? timeSlots.map((slot,index) =>( <option value={slot} key={index} >{slot}</option>)
-             
-             ) :  <option value="select">No class available</option>}
-              
-            
+              {timeSlots.length > 0 ? (
+                timeSlots.map((slot, index) => (
+                  <option
+                    value={slot.slotStart + "-" + slot.slotEnd}
+                    key={index}
+                  >
+                    {slot.slotStart + "-" + slot.slotEnd}
+                  </option>
+                ))
+              ) : (
+                <option value="select">No class available</option>
+              )}
             </select>
           </div>
           <button type="submit" className="btn btn-primary">
@@ -208,11 +256,12 @@ class BookClass extends Component {
     );
   }
 }
+//it will give date in a format which use in html input type of date
 const getDate = (todayDate) => {
   let dd = todayDate.getDate();
   let mm = todayDate.getMonth() + 1; //January is 0!
   let yyyy = todayDate.getFullYear();
-  console.log(todayDate.toString());
+
   // let yyyy = today.getFullYear();
   if (dd < 10) {
     dd = "0" + dd;
@@ -223,7 +272,7 @@ const getDate = (todayDate) => {
   let today = yyyy + "-" + mm + "-" + dd;
   return today;
 };
-
+//it will convert the tim stamp in houre and minutes
 const getSlotInHours = (slots) => {
   let timeSlot = [];
   slots.forEach((slotInfo) => {
@@ -231,17 +280,17 @@ const getSlotInHours = (slots) => {
     let date = new Date(eachSlot);
     // let endTime
     let hours = date.getHours();
+    let slotEndHour = hours + 1;
     // Minutes part from the timestamp
     let minutes = "0" + date.getMinutes();
     // Seconds part from the timestamp
-
+    let slotEndTime = slotEndHour + ":" + minutes.substr(-2);
     // Will display time in 10:30:23 format
     let formattedTime = hours + ":" + minutes.substr(-2);
-  //  console.log(formattedTime);
-    
-    //console.log(date);
-     timeSlot.push(formattedTime);
+    //  console.log(formattedTime);
+    timeSlot.push({ slotStart: formattedTime, slotEnd: slotEndTime });
   });
+  timeSlot.sort();
   return timeSlot;
 };
 export default BookClass;
